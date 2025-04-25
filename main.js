cbcbe4849ca0441af20f4609d86b1fb742da6737
Nayeemnahid19 <nahidnayem19@gmail.com>
2025-04-25 18:19:19 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+/* js/main.js */
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
